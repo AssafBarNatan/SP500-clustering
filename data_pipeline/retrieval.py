8f2bf0091294bc0aa68763860322d26375324690
EMAinasse <47646920+EMAinasse@users.noreply.github.com>
2023-11-29 14:23:12 -0500

    Changes to retrieval

--- data_pipeline/retrieval.py
+++ data_pipeline/retrieval.py
@@ -10,88 +10,111 @@ WIKI_TABLE_URL = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 TODAY = datetime.today()
 
 class DataBank:
-    def __init__(self, table_source = WIKI_TABLE_URL):
+    def __init__(self, 
+                 table_source = WIKI_TABLE_URL, 
+                 table_num = 0, 
+                 ticker_col_name = 'Symbol', 
+                 sector_col_name = 'GICS Sector',
+                 subind_col_name = 'GICS Sub-Industry'):
         self.table_source = table_source
+        self.table_num = table_num
+        self.ticker_col_name = ticker_col_name
+        self.sector_col_name = sector_col_name
+        self.subind_col_name = subind_col_name
 
     def get_table(self, html_source = None) -> pd.DataFrame:
         if html_source is None:
             html_source = WIKI_TABLE_URL
-        return pd.read_html(html_source)[0]
+        return pd.read_html(html_source)[self.table_num]
 
     def get_tickers(self, table = None) -> list[str]:
         if table is None:
             table = self.get_table()
-        return list(table["Symbol"])
+        return list(table[self.ticker_col_name])
 
     def get_sectors_list(self, table = None) -> list[str]:
         if table is None:
             table = self.get_table()
-        return list(table["GICS Sector"].unique())
+        return list(table[self.sector_col_name].unique())
 
     def get_subind_list(self, table = None) -> list[str]:
         if table is None:
             table = self.get_table()
-        return list(list(table["GICS Sub-Industry"].unique()))
+        return list(list(table[self.subind_col_name].unique()))
 
     def get_sector(self, ticker : str, table = None):
         if table is None:
             table = self.get_table()        
-        return table[table.Symbol == ticker]["GICS Sector"].values[0]
+        return table[table.Symbol == ticker][self.sector_col_name].values[0]
 
     def get_subind(self, ticker : str, table = None):
         if table is None:
             table = self.get_table()        
-        return table[table.Symbol == ticker]["GICS Sub-Industry"].values[0]
+        return table[table.Symbol == ticker][self.subind_col_name].values[0]
 
-    def ticker_to_sector_map(self, table = None):
+    def ticker_to_sector_map(self, table = None, tickers = None):
         if table is None:
             table = self.get_table()
         
-        tickers = self.get_tickers(table)
+        if tickers is None:
+            tickers = self.get_tickers(table)
 
         return {
             ticker : self.get_sector(ticker, table)
             for ticker in tickers
             }
     
-    def ticker_to_subind_map(self, table = None):
+    def ticker_to_subind_map(self, table = None, tickers = None):
         if table is None:
             table = self.get_table()
         
-        tickers = DataBank.get_tickers(self, table)
+        if tickers is None:
+            tickers = self.get_tickers(table)
 
         return {
             ticker : self.get_subind(ticker, table)
             for ticker in tickers
             }
 
-    def sector_to_ticker_map(self, table = None):
+    def sector_to_ticker_map(self, table = None, tickers = None):
         if table is None:
             table = self.get_table()
         
-        tickers = DataBank.get_tickers(self, table)
+        if tickers is None:
+            tickers = self.get_tickers(table)
 
-        sectors = DataBank.get_sectors_list(self, table)
+        tick_to_sec_map = self.ticker_to_sector_map(table, tickers)
 
-        return {
-            sector : [ticker for ticker in tickers if self.get_sector(ticker, table) == sector]
-            for sector in sectors
-        }
+        sectors = set(tick_to_sec_map.values())
+
+        sec_to_tick_map = {sector : [] for sector in sectors}
+
+        for ticker in tick_to_sec_map.keys():
+            for sector in sectors:
+                if tick_to_sec_map[ticker] == sector:
+                    sec_to_tick_map[sector].append(ticker)
+
+        return sec_to_tick_map 
     
     def subind_to_ticker_map(self, table = None):
         if table is None:
             table = self.get_table()
         
-        tickers = DataBank.get_tickers(self, table)
+        if tickers is None:
+            tickers = self.get_tickers(table)
 
-        subinds = DataBank.get_sectors_list(self, table)
+        tick_to_sub_map = self.ticker_to_sector_map(table, tickers)
 
-        return {
-            subind : [ticker for ticker in tickers if self.get_subind(ticker, table) == subind]
-            for subind in subinds
-        }
+        subinds = set(tick_to_sub_map.values())
 
-def download_historical_data(tickers : str, start : str, end = TODAY, save_data : bool = True) -> pd.DataFrame:
+        sub_to_tick_map = {subind : [] for subind in subinds}
+
+        for ticker in tick_to_sub_map.keys():
+            for subind in subinds:
+                if tick_to_sub_map[ticker] == subind:
+                    sub_to_tick_map[subind].append(ticker)
+
+def download_historical_data(tickers : list[str], start : str, end = TODAY, save_data : bool = True) -> pd.DataFrame:
     start = datetime.fromisoformat(start)
     
     end = datetime.fromisoformat(end) if not end else end
@@ -107,12 +130,12 @@ def download_historical_data(tickers : str, start : str, end = TODAY, save_data 
     if save_data:
         start = start.strftime('%Y-%m-%d')
         end = end if type(end) == str else end.strftime('%Y-%m-%d')
-        output_path = f"./dataframes/historical_data/SP500_{start}_{end}.pkl"
+        output_path = f"./data/dataframes/historical_data/SP500_{start}_{end}.pkl"
         historical_data.to_pickle(output_path)
     
     return historical_data
 
-def download_adj_close(tickers : str, start : str, end = TODAY, save_data : bool = True) -> pd.DataFrame:
+def download_adj_close(tickers : list[str], start : str, end = TODAY, save_data : bool = True) -> pd.DataFrame:
     """
     Parameter
     ---------
@@ -134,7 +157,7 @@ def download_adj_close(tickers : str, start : str, end = TODAY, save_data : bool
     if save_data:
         start = start
         end = end if type(end) == str else end.strftime('%Y-%m-%d')
-        output_path = f"./dataframes/closing_prices/SP500_{start}_{end}.pkl"
+        output_path = f"./data/dataframes/closing_prices/SP500_{start}_{end}.pkl"
         adj_closing_prices.to_pickle(output_path)
     
     return adj_closing_prices
