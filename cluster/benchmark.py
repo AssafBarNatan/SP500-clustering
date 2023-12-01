2b2a472d4eda8f8f5061a6435d09239a7f7c2798
EMAinasse <47646920+EMAinasse@users.noreply.github.com>
2023-11-30 22:01:10 -0500

    Adding the data in cold storage.

--- cluster/benchmark.py
+++ cluster/benchmark.py
@@ -15,17 +15,21 @@ class SectorCluster:
         self.ticker_col_name = ticker_col_name
         self.sector_col_name = sector_col_name
 
-    def fit(self, tickers):
+    def ticker_to_sector_map(self, tickers):
+        if not (isinstance(tickers, list) and all([isinstance(ticker, str) for ticker in tickers])):
+            raise ValueError("The input must be a list of tickers.")        
+        
         table = pd.read_html(self.table_source)[self.table_num]
 
-        ticker_to_sector_map = table[
-            table['Symbol'].isin(tickers)
-            ][
-                [self.ticker_col_name, self.sector_col_name]
-                ].set_index(self.ticker_col_name).to_dict()[self.sector_col_name]
-        
+        return table[table[self.ticker_col_name].isin(tickers)][
+            [self.ticker_col_name, self.sector_col_name]
+            ].set_index(self.ticker_col_name).to_dict()[self.sector_col_name]
+    
+    def fit(self, df):
+        tickers = list(df.columns)
+
         self.labels_ = np.array(
-            list(ticker_to_sector_map.values())
+            list(self.ticker_to_sector_map(tickers).values())
         )
     
         return self
@@ -42,17 +46,21 @@ class SubindustryCluster:
         self.ticker_col_name = ticker_col_name
         self.subind_col_name = subind_col_name
 
-    def fit(self, tickers):
+    def ticker_to_subind_map(self, tickers):
+        if not (isinstance(tickers, list) and all([isinstance(ticker, str) for ticker in tickers])):
+            raise ValueError("The input must be a list of tickers.")        
+        
         table = pd.read_html(self.table_source)[self.table_num]
 
-        ticker_to_subind_map = table[
-            table['Symbol'].isin(tickers)
-            ][
-                [self.ticker_col_name, self.subind_col_name]
-                ].set_index(self.ticker_col_name).to_dict()[self.subind_col_name]
+        return table[table[self.ticker_col_name].isin(tickers)][
+            [self.ticker_col_name, self.subind_col_name]
+            ].set_index(self.ticker_col_name).to_dict()[self.subind_col_name]
+    
+    def fit(self, df):
+        tickers = df.columns
         
         self.labels_ = np.array(
-            list(ticker_to_subind_map.values())
+            list(self.ticker_to_subind_map(tickers).values())
         )
     
         return self
